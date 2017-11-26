fb6cc5baeb3b8cb2c80f452dfd8dd105add70544
bwyap <bwyap@outlook.com>
2017-11-27 09:55:23 +1100

    [REFACTOR] Move Semantic UI folder into src folder and renamed it to semantic-ui

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
 
-import './styles/semantic.scss';
+import './app/styles/semantic.scss';
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
