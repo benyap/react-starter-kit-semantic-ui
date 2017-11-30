0d9c8b4227bb854390748cfe5647e601c7012f20
bwyap <bwyap@outlook.com>
2017-11-30 15:47:37 +1100

    [ADD] Update footer links

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -9,7 +9,10 @@ export class Footer extends React.Component {
 			<div className='component-footer'>
 				<div className='component-footer__text'>
 					<span>
-						&copy; 2017 // assembled by bwyap // <a href='https://github.com/bwyap'>GitHub</a>
+						&copy; 2017 // assembled by <a href='https://github.com/bwyap'>bwyap</a>
+					</span>
+					<span>
+						<a href='https://github.com/bwyap/react-starter-kit-semantic-ui/blob/master/LICENSE'>License</a>
 					</span>
 				</div>
 			</div>
